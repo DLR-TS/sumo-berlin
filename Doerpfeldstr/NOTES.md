02819af70eaeb04181c1c5a53ea3a2c9fe004c36
Michael Behrisch <oss@behrisch.de>
2026-05-18 21:46:55 +0200

    backing up current state

--- Doerpfeldstr/NOTES.md
+++ Doerpfeldstr/NOTES.md
@@ -7,3 +7,17 @@
   - turn probabilities (see turn_probabilities.xml)
   - major routes driven (see main_relations.xml)
 - in the end if the inputs are not correctly matched it will not help
+- if you see unusual lane usgae, check connectivity and also priority of connections
+- avoid stops close to junctions if it is possible that the stopping vehicle needs to change lanes before the junction (e.g. to turn left)
+- if long vehicles brake to early for oncoming traffic, try to set keepclear to false
+- do not look only at teleports, cars may also be stuck in the insertion backlog
+- enable edgedata, color by live edgedata teleports and look at the live teleport increasement
+- does it have any effect if I connect the streets to the bike lanes and vice versa?
+
+
+# Improvement ideas
+- routeSampler should use the preferences and maybe routing costs in general to weight the alternatives
+
+
+# Bugs
+- when loading edge data with different attributes and switching coloring and then back to edge data, sumo-gui crashes with "Error: String 'intermediate' not found."
--- Doerpfeldstr/buildRoutes.sh
+++ Doerpfeldstr/buildRoutes.sh
@@ -5,21 +5,22 @@ python $SUMO_HOME/tools/import/gtfs/gtfs2pt.py -c Doerpfeldstr.gtfscfg
 # cars
 python $SUMO_HOME/tools/randomTrips.py -n Doerpfeldstr_edit.net.xml.gz --seed 42 --fringe-factor 8 -p 1  -r cars.rou.xml --prefix veh --lanes -l --min-distance 300 --remove-loops -a preferences.xml
 python ./telraam2meandata.py -c Doerpfeldstr.t2mdcfg -o telraam_car_data.xml
-python $SUMO_HOME/tools/routeSampler.py -r cars.rou.xml -d telraam_car_data.xml,manual_car_data.xml -o cars_sampled.rou.xml --prefix car_ --mismatch-output mismatch_car.xml --timeline TGw2_PKW --merge-strategy ignore
+python $SUMO_HOME/tools/routeSampler.py -r cars.rou.xml -d telraam_car_data.xml,manual_car_data.xml -o cars_sampled.rou.xml --prefix car_ --mismatch-output mismatch_car.xml --timeline TGw2_PKW --merge-strategy ignore -T turn_probabilities.xml --attributes "color=\"random\""
+# --turn-files main_relations.xml
 python $SUMO_HOME/tools/countEdgeUsage.py cars_sampled.rou.xml -i
 python $SUMO_HOME/tools/route/addStops2Routes.py -c Doerpfeldstr.as2rcfg --route-files cars_sampled.rou.xml -o cars_with_stops.rou.xml
 # bikes
-python $SUMO_HOME/tools/randomTrips.py -n Doerpfeldstr_edit.net.xml.gz --seed 42 --fringe-factor 10 -p 5  -r bikes.rou.xml -b 50400 -e 54000 --vehicle-class bicycle --vclass bicycle --prefix  bike --prefix "b" --lanes -l
+python $SUMO_HOME/tools/randomTrips.py -n Doerpfeldstr_edit.net.xml.gz --seed 42 --fringe-factor 10 -p 5  -r bikes.rou.xml --vehicle-class bicycle --vclass bicycle --prefix bike --lanes -l
 python ./telraam2meandata.py -c Doerpfeldstr.t2mdcfg -o telraam_bike_data.xml --vtype bike
-python $SUMO_HOME/tools/routeSampler.py -r bikes.rou.xml -d telraam_bike_data.xml -o bikes_sampled.rou.xml --prefix bike_ --mismatch-output mismatch_bike.xml --attributes "type=\"bicycle\""
+python $SUMO_HOME/tools/routeSampler.py -r bikes.rou.xml -d telraam_bike_data.xml -o bikes_sampled.rou.xml --prefix bike --mismatch-output mismatch_bike.xml --attributes "type=\"bicycle\" color=\"random\""
 # delivery
-python $SUMO_HOME/tools/randomTrips.py -n Doerpfeldstr_edit.net.xml.gz --seed 42 --fringe-factor 8 -p 8  -r delivery.rou.xml -b 50400 -e 54000 --vehicle-class delivery --vclass delivery --prefix deliv --lanes -l --min-distance 300 --remove-loops -a preferences.xml
+python $SUMO_HOME/tools/randomTrips.py -n Doerpfeldstr_edit.net.xml.gz --seed 42 --fringe-factor 8 -p 8  -r delivery.rou.xml --vehicle-class delivery --vclass delivery --prefix deliv --lanes -l --min-distance 300 --remove-loops -a preferences.xml
 python ./telraam2meandata.py -c Doerpfeldstr.t2mdcfg -o telraam_heavy_data.xml --vtype heavy
-python $SUMO_HOME/tools/routeSampler.py -r delivery.rou.xml -d telraam_heavy_data.xml -o delivery_sampled.rou.xml --prefix delivery_ --mismatch-output mismatch_delivery.xml --attributes "type=\"delivery\"" --timeline TGw_LKW --merge-strategy ignore
+python $SUMO_HOME/tools/routeSampler.py -r delivery.rou.xml -d telraam_heavy_data.xml -o delivery_sampled.rou.xml --prefix delivery --mismatch-output mismatch_delivery.xml --attributes "type=\"delivery\"" --timeline TGw_LKW --merge-strategy ignore
 python $SUMO_HOME/tools/route/addStops2Routes.py -c Doerpfeldstr.as2rcfg --route-files delivery_sampled.rou.xml -o delivery_with_stops.rou.xml -t vtypes.xml
 # pedestrians
-python $SUMO_HOME/tools/randomTrips.py -n Doerpfeldstr_edit.net.xml.gz --seed 42 --fringe-factor 1 --insertion-density 10 -r peds.rou.xml -b 50400 -e 54000 --vehicle-class pedestrian --prefix ped --pedestrians --max-distance 2000 --persontrip.walk-opposite-factor 0.8
+python $SUMO_HOME/tools/randomTrips.py -n Doerpfeldstr_edit.net.xml.gz --seed 42 --fringe-factor 1 --insertion-density 10 -r peds.rou.xml --vehicle-class pedestrian --prefix ped --pedestrians --max-distance 2000 --persontrip.walk-opposite-factor 0.8 --persontrip.modes public -a vtypes.xml,gtfs_pt_stops.add.xml,gtfs_pt_vehicles.add.xml
 python ./telraam2meandata.py -c Doerpfeldstr.t2mdcfg -o telraam_ped_data.xml --vtype ped
-python $SUMO_HOME/tools/routeSampler.py -r peds.rou.xml -d telraam_ped_data.xml -o ped_sampled.rou.xml --prefix ped_ --mismatch-output mismatch_ped.xml --pedestrians --attributes "type=\"pedestrian\""
+python $SUMO_HOME/tools/routeSampler.py -r peds.rou.xml -d telraam_ped_data.xml -o ped_sampled.rou.xml --prefix ped --mismatch-output mismatch_ped.xml --pedestrians --attributes "type=\"pedestrian\" color=\"random\""
 
 popd
